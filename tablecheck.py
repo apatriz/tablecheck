c954d6beca5dff93103610c746fc313355e74b1f
apatriz <patrizio@msu.edu>
2016-02-03 15:45:25 -0500

    added tablecheck.py

---
+++ tablecheck.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import arcpy,os
+from openpyxl import load_workbook
+
+EXCEL_FILE = r"C:\Users\patrizio\Projects\Monroe_Signs\test\Lookup_Table"
+
+"""
+1. read Excel file into dictionary
+2. use arcpy update cursor to iterate through each table record (use only fields
+in Excel table to increase cursor speed)
+3. if any row value is None, get standard field value from 'Descrip' key and field name.
+4. update row and move to next
+
+"""
+
+def load_excel(filename):
+    res = {}
+    wb = load_workbook(filename=filename,use_iterators=True)
+    ws = wb.active
+    header_range = "A1:S1"
+    headers = []
+
+    #get headers
+    for row in ws.iter_rows(range_string=header_range):
+        for cell in row:
+            headers.append(cell.value)
+    return headers
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    print load_excel(EXCEL_FILE)
