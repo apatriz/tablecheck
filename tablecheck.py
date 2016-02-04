e0450535854d695fc0c8379f021b8dcd4573a93a
apatriz <patrizio@msu.edu>
2016-02-04 16:43:32 -0500

    failing on check_arc_table

--- tablecheck.py
+++ tablecheck.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 
-import arcpy,os, csv
+import arcpy,os
+from openpyxl import load_workbook
+from openpyxl.utils import column_index_from_string
 
-EXCEL_FILE = r"C:\Users\patrizio\Projects\Monroe_Signs\test\Lookup_Table"
+EXCEL_FILE = r"C:\Users\patrizio\Projects\Monroe_Signs\test\Lookup_Table.xlsx"
+FIELD_NAME_RANGE = "A1:S1"
+ID_COLUMN = "B"
+
+DATABASE_TABLE = r"C:\Users\patrizio\Projects\Monroe_Signs\test\Monroe_Signs.gdb\Signs"
+FIELD_MAP = {"SignType":"Descrip",
+             "DimHeight":"DimHeight",
+             "DimWidth":"DimWidth",
+             "LegendColor1":"LegendColor1",
+             "LegendColor2":"LegendColor2",
+             "LegendColor3":"LegendColor3",
+             "SheetColor1":"SheetingColor1",
+             "SheetColor2":"SheetingColor2",
+             "RegPkRestr1":"RegPkRestrType1",
+             "RegPkRestr2":"RegPkRestrType2",
+             "RegPkTimeLim1":"RegPkTimeLimit1",
+             "RegPkTimeLim2":"RegPkTimeLimit2",
+             "RegPkArrow1":"RegPkArrow1",
+             "RegPkTimeYear1":"RegPkTimeYear1",
+             "RegPkTimeYear2":"RegPkTimeYear2",
+             "RegPkVehExcep1":"RegPkVehExceptions1",
+             "RegPkVehExcep2":"RegPkVehExceptions1",
+             }
+             
 
 """
-1. read Excel file into dictionary
+1. read Excel file into dictionary - DONE
 2. use arcpy update cursor to iterate through each table record (use only fields
 in Excel table to increase cursor speed)
 3. if any row value is None, get standard field value from 'Descrip' key and field name.
@@ -13,23 +38,73 @@ in Excel table to increase cursor speed)
 
 """
 
-def load_excel(filename):
-    res = {}
-    wb = load_workbook(filename=filename,use_iterators=True)
+#decorator to convert indexes to 0-based
+def convertindex(func):
+    def minus_one(index_string):
+        return func(index_string) - 1
+    return minus_one
+get_index = convertindex(column_index_from_string)
+
+    
+
+def get_headers(filename,field_range):
+    """
+    Input:
+    filename -> path to excel file
+    field_range -> range of cell coordinates for getting headers, e.g. 'A1:S1'
+
+    Output: list
+    """
+    wb = load_workbook(filename=filename,read_only=True)
     ws = wb.active
-    header_range = "A1:S1"
     headers = []
 
-    #get headers
-    for row in ws.iter_rows(range_string=header_range):
+    for row in ws.iter_rows(range_string=field_range):
         for cell in row:
             headers.append(cell.value)
     return headers
-    
-    
-    
-    
-    
+
+
+def load_dict(filename,headers,key_id):
+    wb = load_workbook(filename=filename,read_only=True)
+    ws = wb.active
+    res = {}
+    for row in ws.iter_rows(row_offset=1):
+        index = 0
+        entry = {}     
+        for cell in row:
+            entry[headers[index]] = cell.value
+            index += 1
+        res[row[key_id].value] = entry        
+    return res
+
+def check_arc_table(filename,id_field_name,field_map,lookup_table):
+    fields = [field.name for field in arcpy.ListFields(filename) if field.name in field_map.values()]
+    with arcpy.da.UpdateCursor(filename,fields) as cursor:
+        for row in cursor:
+            index = 0
+            for cell in row:
+                if fields[index] == id_field_name and cell:
+                    lookup_id = cell
+                    print lookup_id
+                elif fields[index] == id_field_name and not cell:
+                    break
+                if not cell:  
+                    lookup_value = field_map[cursor.fields[index]]                 
+                    cell = lookup_table[lookup_id][lookup_value]
+                index += 1
+            cursor.updateRow(row)
+                
+        
+              
     
 if __name__ == "__main__":
-    print load_excel(EXCEL_FILE)
+    headers = get_headers(EXCEL_FILE,FIELD_NAME_RANGE)
+    key_id = get_index(ID_COLUMN)
+    print headers
+    lookup_table = load_dict(EXCEL_FILE,headers,key_id)
+##    print lookup_table["D4-3"]
+
+    check_arc_table(DATABASE_TABLE,"SignType",FIELD_MAP,lookup_table)
+    
+    
